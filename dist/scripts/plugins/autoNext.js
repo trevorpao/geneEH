e0609b819a29c06813bade22f901e49e4acce6d4
Trevor Pao <shuaib25@gmail.com>
2017-04-12 21:01:51 +0800

    add dist folder

---
+++ dist/scripts/plugins/autoNext.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * 多語系切換按鈕
+ */
+;(function (gee, $) {
+    'use strict';
+
+    gee.hook('autoNext', function (me) {
+        var $ta = $(me.data('ta')),
+            v = me.val();
+
+        if (v.length == me.attr('maxlength')) {
+            if ($ta.length) {
+                $ta.focus().select();
+            } else {
+                me.next('input').focus().select();
+            }
+        }
+    }, 'keyup');
+})(gee, jQuery);
